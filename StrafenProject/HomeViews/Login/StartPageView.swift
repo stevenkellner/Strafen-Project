6a0f5a34ef336696a83573c0c82d2a6eceb6774d
Steven Kellner <steven.kellner@web.de>
2023-04-12 16:44:41 +0200

    Created start page

---
+++ StrafenProject/HomeViews/Login/StartPageView.swift
@@ -0,0 +1,79 @@
+//
+//  StartPageView.swift
+//  StrafenProject
+//
+//  Created by Steven on 11.04.23.
+//
+
+import SwiftUI
+
+struct StartPageView: View {
+    @State var isTermsAndPrivacySheetShown = false
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                self.backgroundImage
+                    .ignoresSafeArea(edges: .top)
+                VStack {
+                    Spacer()
+                    self.startAndLoginButtons
+                        .padding(.bottom, 25)
+                    self.termsAndPrivacy
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder var backgroundImage: some View {
+        VStack {
+            Image("startPage-background")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Spacer()
+        }
+    }
+    
+    @ViewBuilder var startAndLoginButtons: some View {
+        VStack {
+            NavigationLink(destination: Text("TODO")) {
+                Text("start-page|buttons|start-register-create-club", comment: "Start button on start page to get to register / create club page.")
+                    .font(.title2)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 35)
+            }.buttonStyle(.borderedProminent)
+            
+            NavigationLink(destination: Text("TODO")) {
+                Text("start-page|buttons|login", comment: "Login button on start page to get to login page.")
+                    .font(.title2)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 35)
+            }.buttonStyle(.bordered)
+        }.padding(.horizontal)
+    }
+    
+    @ViewBuilder var termsAndPrivacy: some View {
+        VStack {
+            Text("agree-terms-privacy-by-continue|by-continue-agree", comment: "Starting part of 'By continuing you agree to Terms, Conditions and Privacy Policy', i.e. 'By continuing you agree to'. Don't add trailing space as it's splitted between two lines.")
+                .foregroundColor(.secondary)
+            Button(role: .none) {
+                self.isTermsAndPrivacySheetShown = true
+            } label: {
+                Text("agree-terms-privacy-by-continue|terms-and-privacy", comment: "Starting part of 'By continuing you agree to Terms, Conditions and Privacy Policy', i.e. 'Terms, Conditions and Privacy Policy'.")
+            }.buttonStyle(.borderless)
+        }.sheet(isPresented: self.$isTermsAndPrivacySheetShown) {
+            NavigationView {
+                TermsAndPrivacyView()
+                    .toolbar {
+                        ToolbarItem {
+                            Button {
+                                self.isTermsAndPrivacySheetShown = false
+                            } label: {
+                                Text("dismiss-sheet", comment: "Dismiss button on a sheet.")
+                            }.buttonStyle(.borderless)
+                        }
+                    }
+            }
+        }
+    }
+}
---
+++ StrafenProject/HomeViews/Login/TermsAndPrivacyView.swift
@@ -0,0 +1,14 @@
+//
+//  TermsAndPrivacyView.swift
+//  StrafenProject
+//
+//  Created by Steven on 12.04.23.
+//
+
+import SwiftUI
+
+struct TermsAndPrivacyView: View {
+    var body: some View {
+        EmptyView() // TODO
+    }
+}
